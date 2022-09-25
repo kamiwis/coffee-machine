f772b0811edc15f307cc8c87ddc7360b7829223b
Kamila Wisniewska <kmlwisniewska@gmail.com>
2022-09-25 19:59:28 -0400

    Added process_coins() function to helpers. Added statements checking users choice.

--- helpers.py
+++ helpers.py
@@ -1,16 +1,22 @@
 from menu import MENU, resources
 
 def prompt_user():
+    """Prompts user for input and returns user's choice"""
     choice = input("What would you like? (espresso/latte/cappuccino): ")
     return choice
 
 def print_report(resources):
+    """Takes resources dictionary as parameter and prints a report detailing the amount of resources
+    left in the coffee machine."""
     print(f"Water: {resources['water']}ml")
     print(f"Milk: {resources['milk']}ml")
     print(f"Coffee: {resources['coffee']}")
     print(f"Money: ${resources['money']}")
 
 def check_resources(drink, resources):
+    """Takes user's drink choice and resources dictionary as input.
+    Checks to see if there are enough resoures in the machine to make the user's drink choice.
+    If even one resources is lower than what the user needs to make their drink, returns False."""
     enough = True
     # Check water
     if MENU[drink]["ingredients"]["milk"] > resources["milk"]:
@@ -25,4 +31,12 @@ def check_resources(drink, resources):
         print("Sorry there is not enough coffee.")
         enough = False
     return enough
+
+def process_coins():
+    quarters = int(input("How many quarters?: "))
+    dimes = int(input("How many dimes?: "))
+    nickles = int(input("How many nickles?: "))
+    pennies = int(input("How many pennies?: "))
+    total = (quarters * 0.25) + (dimes * 0.1) + (nickles * 0.05) + (pennies * 0.01)
+    return total
     
--- main.py
+++ main.py
@@ -1,19 +1,28 @@
+import os
 from menu import MENU, resources
-from helpers import prompt_user, print_report, check_resources, 
+from helpers import prompt_user, print_report, check_resources, process_coins
 
 def main():
 
     # Prompt user - every time an action is completed.
     user_choice = prompt_user()
-    # TODO Check user input.
 
-# TODO If user enters 'off' to prompt - code will end execution.
+    # If user enters 'off' to prompt - code will end execution.
+    if user_choice == "off":
+        os.system("clear")
+        return
+    # If user enters 'report' to prompt - print a report of all resources and total money.
+    if user_choice == "report":
+        print_report(resources)
 
-# TODO If user enters 'report' to prompt - print a report of all resources and total money.
-
-# TODO If user enters a drink, check if resources are sufficient for that drink.
-
-# TODO If resources are not sufficient, give feedback on what is low
+    #  If user enters a drink, check if resources are sufficient for that drink.
+    if user_choice in ["espresso", "latte", "cappuccino"]:
+        enough_resources = check_resources(user_choice, resources)
+        if enough_resources:
+            print("Please insert coins.")
+            total_coins = process_coins()
+            if total_coins == MENU[user_choice]["cost"]:
+                print(f"Here is your {user_choice} ☕ Enjoy!")
 
 # TODO Process coins if resources are sufficient.
 
@@ -24,4 +33,4 @@ def main():
 # TODO If user inputs too much money, machine offers change.
 
 
-
+main()
