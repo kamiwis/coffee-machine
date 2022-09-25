19921b842755ed072340aa9b05c80f792998c4ba
Kamila Wisniewska <kmlwisniewska@gmail.com>
2022-09-25 19:21:55 -0400

    Added TODO statements. Created menu.py to store menu and resources.

---
+++ main.py
@@ -0,0 +1,25 @@
+from menu import MENU, resources
+
+
+# TODO Prompt user - every time an action is completed.
+
+# TODO Check user input.
+
+# TODO If user enters 'off' to prompt - code will end execution.
+
+# TODO If user enters 'report' to prompt - print a report of all resources and total money.
+
+# TODO If user enters a drink, check if resources are sufficient for that drink.
+
+# TODO If resources are not sufficient, give feedback on what is low
+
+# TODO Process coins if resources are sufficient.
+
+# TODO If user doesn't input enough money, refund money.
+
+# TODO If user inputs enough money, cost of drink gets added to machine as profit.
+
+# TODO If user inputs too much money, machine offers change.
+
+
+
